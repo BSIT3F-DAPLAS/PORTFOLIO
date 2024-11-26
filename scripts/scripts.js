631370d4e2687d4e1878be0b4aae260afca2bdda
unknown <dashanam.villanueva@gmail.com>
2024-11-26 17:30:13 +0800

    PORTFOLIO

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+// Placeholder JavaScript for future interactivity
+document.querySelectorAll('.portfolio-item').forEach(item => {
+  item.addEventListener('click', () => {
+      alert("Hi I'm Danica!");
+  });
+});
